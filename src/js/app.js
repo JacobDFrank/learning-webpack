1478854118fa885acbddc1cbf2c67eaa076cab7b
JacobDFrank <frankjacob42@gmail.com>
2017-10-23 17:58:21 -0400

    Added current works

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import '../styles/style.scss';
+
+import starfishImage from '../assets/starFish.jpg'; // Importing image -> ADDED IN THIS STEP
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <div>
+        Hello from react
+
+        {/* ADDED IN THIS STEP */}
+        <img class="image" src={ starfishImage } alt='Commander Keen' />
+      </div>
+    );
+  }
+}
+
+render(<Hello />, document.getElementById('app'));
